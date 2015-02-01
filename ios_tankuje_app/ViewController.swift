6aaaea53785b0466cf238a957347890ebb7757f6
Grzegorz Gurzeda <grzegorzgurzeda@MBP-Grzegorz.lan>
2015-02-01 19:02:34 +0100

    Initial commit

---
+++ ios_tankuje_app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios_tankuje_app
+//
+//  Created by Grzegorz Gurzeda on 01.02.2015.
+//  Copyright (c) 2015 Grzegorz Gurzeda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
